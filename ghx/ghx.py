13ee74b3d6130c16e4c4ec32f63b93548972739c
Matt Mitchell <matt.s.mitchell@okstate.edu>
2016-09-30 14:22:57 -0500

    Load aggregation objects are being created and collapse to larger blocks at appropriate times.

--- ghx/ghx.py
+++ ghx/ghx.py
@@ -40,7 +40,7 @@ class GHXArray:
         GHXArray(<json_path>, <loads_path>)
         """
 
-        ## Class data
+        # class data
 
         self.name = ""
         self.num_bh = 0
@@ -78,7 +78,7 @@ class GHXArray:
         self.agg_hour = 0
         self.sim_hour = 0
 
-        ## initialize methods
+        # initialize methods
 
         # get ghx data
         self.get_input(json_path)
@@ -93,7 +93,7 @@ class GHXArray:
         self.set_load_aggregation()
 
         # set first aggregated load, which is zero. Need this for later
-        self.agg_load_objects.append(AggregatedLoad([0], 0))
+        self.agg_load_objects.append(AggregatedLoad([0], 0, 0))
 
     def get_input(self, json_path):
 
@@ -114,7 +114,7 @@ class GHXArray:
 
             if self.print_output: print("....Success")
 
-        except ValueError: # pragma: no cover
+        except ValueError:  # pragma: no cover
             if self.print_output: print("Error reading JSON data file---check file path")
             if self.print_output: print("Program exiting")
             sys.exit(1)
@@ -127,55 +127,55 @@ class GHXArray:
 
             try:
                 self.name = json_data['Name']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Name' key not found")
                 pass
 
             try:
                 self.num_bh = json_data['Number BH']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Number BH' key not found")
                 pass
 
             try:
                 self.flow_rate = json_data['Flow Rate']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Flow Rate' key not found")
                 pass
 
             try:
                 self.ground_cond = json_data['Ground Cond']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Ground Cond' key not found")
                 pass
 
             try:
                 self.ground_heat_capacity = json_data['Ground Heat Capacity']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Ground Heat Capacity' key not found")
                 pass
 
             try:
                 self.ground_temp = json_data['Ground Temp']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Ground Temp' key not found")
                 pass
 
             try:
                 self.fluid = json_data['Fluid']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Fluid' key not found")
                 pass
 
             try:
                 self.sim_years = json_data['Simulation Years']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Simulation Years' key not found")
                 pass
 
             try:
                 self.aggregation_type = json_data['Aggregation Type']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Aggregation Type' key not found")
                 pass
 
@@ -183,7 +183,7 @@ class GHXArray:
                 self.g_func_pairs = json_data['G-func Pairs']
                 self.g_func_present = True
                 self.update_g_func_interp_lists()
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'G-func Pairs' key not found")
                 pass
 
@@ -192,7 +192,7 @@ class GHXArray:
 
             # success
             if self.print_output: print("....Success")
-        except ValueError: # pragma: no cover
+        except ValueError:  # pragma: no cover
             if self.print_output: print("Error loading data into data structs")
             if self.print_output: print("Program exiting")
             sys.exit(1)
@@ -217,55 +217,55 @@ class GHXArray:
             # import GHX data
             try:
                 self.ghx_list[i].name = json_data['GHXs'][i]['Name']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Name' key not found")
                 pass
 
             try:
                 self.ghx_list[i].location = json_data['GHXs'][i]['Location']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Location' key not found")
                 pass
 
             try:
                 self.ghx_list[i].bh_length = json_data['GHXs'][i]['BH Length']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'BH Length' key not found")
                 pass
 
             try:
                 self.ghx_list[i].bh_radius = json_data['GHXs'][i]['BH Radius']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'BH Radius' key not found")
                 pass
 
             try:
                 self.ghx_list[i].grout_cond = json_data['GHXs'][i]['Grout Cond']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Grout Cond' key not found")
                 pass
 
             try:
                 self.ghx_list[i].pipe_cond = json_data['GHXs'][i]['Pipe Cond']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 print("\t'Pipe Cond' key not found")
                 pass
 
             try:
                 self.ghx_list[i].pipe_out_dia = json_data['GHXs'][i]['Pipe Dia']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Pipe Dia' key not found")
                 pass
 
             try:
                 self.ghx_list[i].shank_space = json_data['GHXs'][i]['Shank Space']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Shank Space' key not found")
                 pass
 
             try:
                 self.ghx_list[i].pipe_thickness = json_data['GHXs'][i]['Pipe Thickness']
-            except ValueError: # pragma: no cover
+            except ValueError:  # pragma: no cover
                 if self.print_output: print("\t'Pipe Thickness' key not found")
                 pass
 
@@ -282,10 +282,26 @@ class GHXArray:
             self.load_pairs = np.genfromtxt(load_path, delimiter=',', names=True)
             self.update_load_lists()
             if self.print_output: print("....Success")
-        except ValueError: # pragma: no cover
+        except ValueError:  # pragma: no cover
             if self.print_output: print("Error importing loads")
             if self.print_output: print("Program exiting")
             sys.exit(1)
+
+    def calc_ts(self):
+
+        """
+        Calculates non-dimensional time. Selects length scale based on deepest GHX
+        """
+
+        max_h = 0.0
+
+        self.ground_thermal_diff = self.ground_cond / self.ground_heat_capacity
+
+        for i in range(self.num_bh):
+            if max_h < self.ghx_list[i].bh_length:
+                max_h = self.ghx_list[i].bh_length
+
+        self.ts = max_h ** 2 / (9 * self.ground_thermal_diff)
 
     def set_load_aggregation(self):
 
@@ -336,7 +352,7 @@ class GHXArray:
             self.g_func_present = True
             self.update_g_func_interp_lists()
             if self.print_output: print("....Success")
-        except ValueError: # pragma: no cover
+        except ValueError:  # pragma: no cover
             if self.print_output: print("Error calculating g-functions")
             if self.print_output: print("Program exiting")
             sys.exit(1)
@@ -398,9 +414,11 @@ class GHXArray:
         Creates aggregated load object
         """
 
-        self.agg_load_objects.append(AggregatedLoad(self.hourly_loads, sim_hour))
-        self.collapse_aggregate_loads()
-        self.agg_hour = 0
+        if len(self.agg_load_intervals) > 1:
+            self.collapse_aggregate_loads()
+
+        prev_sim_hour = self.agg_load_objects[-1].last_sim_hour
+        self.agg_load_objects.append(AggregatedLoad(self.hourly_loads, sim_hour, prev_sim_hour))
 
     def collapse_aggregate_loads(self):
 
@@ -408,21 +426,62 @@ class GHXArray:
         Collapses aggregated loads
         """
 
-    def calc_ts(self):
+        agg_load_objects_update = []
+
+        i = 0
+        while i < len(self.agg_load_objects):
+            if i == 0:  # keep '0' time object
+                agg_load_objects_update.append(self.agg_load_objects[i])
+                i += 1
+                continue
+            elif len(self.agg_load_objects[i].loads) == self.agg_load_intervals[-1]:  # already max agg interval
+                agg_load_objects_update.append(self.agg_load_objects[i])
+                i += 1
+                continue
+            else:
+                k = len(self.agg_load_intervals) - 1
+                while k >= 0:
+                    temp_objs = []
+                    agg_int = self.agg_load_intervals[k]
+                    for j in range(i, len(self.agg_load_objects)):
+                        if len(self.agg_load_objects[j].loads) == agg_int:
+                            temp_objs.append(self.agg_load_objects[j])
+                        else:
+                            continue
+                    num_objs = len(temp_objs)
+                    i += num_objs
+                    if num_objs > 0:
+                        if num_objs*agg_int >= self.agg_load_intervals[k+1]:
+                            agg_load_objects_update.append(self.merge_agg_load_objs(temp_objs))
+                        else:
+                            for l in range(len(temp_objs)):
+                                agg_load_objects_update.append(temp_objs[l])
+                    k -= 1
+
+
+        self.agg_load_objects = agg_load_objects_update
+
+    def merge_agg_load_objs(self, obj_list):
 
         """
-        Calculates non-dimensional time. Selects length scale based on deepest GHX
+        Merges AggregatedLoad objects into a single AggregatedLoad object
+
+        :return: merged AggregatedLoad object
         """
 
-        max_h = 0.0
+        loads = []
+        min_hour = hours_in_year * self.sim_years
+        max_hour = 0
 
-        self.ground_thermal_diff = self.ground_cond / self.ground_heat_capacity
+        for this_obj in obj_list:
+            for i in range(len(this_obj.loads)):
+                loads.append(this_obj.loads[i])
+            if min_hour > this_obj.first_sim_hour:
+                min_hour = this_obj.first_sim_hour
+            if max_hour < this_obj.last_sim_hour:
+                max_hour = this_obj.last_sim_hour
 
-        for i in range(self.num_bh):
-            if max_h < self.ghx_list[i].bh_length:
-                max_h = self.ghx_list[i].bh_length
-
-        self.ts = max_h ** 2 / (9 * self.ground_thermal_diff)
+        return AggregatedLoad(loads, max_hour, min_hour)
 
     def generate_output_reports(self):
 
@@ -497,7 +556,8 @@ class GHXArray:
                         q_cur_hour = self.hourly_loads[index_cur_hour]
                         q_prev_hour = self.hourly_loads[index_prev_hour]
                         g = self.g_func_hourly[i]
-                        temp_bh_hourly.append((q_cur_hour - q_prev_hour) / (2 * np.pi * self.ground_cond * self.num_bh * self.ghx_list[0].bh_length) * g)
+                        temp_bh_hourly.append((q_cur_hour - q_prev_hour) /
+                                              (2 * np.pi * self.ground_cond * self.num_bh * self.ghx_list[0].bh_length) * g)
 
                     # aggregated load effects
                     temp_bh_agg = []
@@ -507,20 +567,26 @@ class GHXArray:
                         curr_obj = self.agg_load_objects[i]
                         prev_obj = self.agg_load_objects[i-1]
 
-                        t = self.sim_hour - ((curr_obj.time() + prev_obj.time()) / 2.0)
-                        ln_t_ts = np.log(t * 3600 / self.ts)
-
+                        t_agg = self.sim_hour - ((curr_obj.time() + prev_obj.time()) / 2.0)
+                        ln_t_ts = np.log(t_agg * 3600 / self.ts)
                         g = self.g_func(ln_t_ts)
-
-                        temp_bh_agg.append((curr_obj.q - prev_obj.q) / (2 * np.pi * self.ground_cond * self.num_bh * self.ghx_list[0].bh_length) * g)
+                        temp_bh_agg.append((curr_obj.q - prev_obj.q) /
+                                           (2 * np.pi * self.ground_cond * self.num_bh * self.ghx_list[0].bh_length) * g)
 
                     # final bh temp
                     self.temp_bh.append(self.ground_temp + sum(temp_bh_hourly) + sum(temp_bh_agg))
 
                     # aggregate load
                     if self.agg_hour == self.agg_load_intervals[0]:
+                        # this has one extra value for comparative purposes
+                        # need to get rid of it here
                         self.hourly_loads.popleft()
+
+                        # create new aggregated load object
                         self.aggregate_load(self.sim_hour)
+
+                        # reset aggregation hour to '0'
+                        self.agg_hour = 0
 
         self.generate_output_reports()
 
@@ -576,14 +642,16 @@ class AggregatedLoad:
     Class that contains a block of aggregated loads
     """
 
-    def __init__(self, loads, sim_hour):
+    def __init__(self, loads, last_sim_hour, first_sim_hour):
 
         """
         Constructor for the class
         """
 
         self.loads = deque(loads,maxlen=len(loads))
-        self.sim_hour = sim_hour
+        self.last_sim_hour = last_sim_hour
+        self.first_sim_hour = first_sim_hour
+        self.ave_sim_hour = (last_sim_hour + first_sim_hour) / 2.0
         self.q = np.mean(self.loads)
 
     def time(self):
@@ -592,4 +660,4 @@ class AggregatedLoad:
         :returns absolute time (in hours) when load occurred
         """
 
-        return self.sim_hour
+        return self.ave_sim_hour
--- test/test_ghx.py
+++ test/test_ghx.py
@@ -1,6 +1,7 @@
 
 import os
 import sys
+from collections import deque
 
 # add the source directory to the path so the unit test framework can find it
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'ghx'))
@@ -58,10 +59,6 @@ class TestGHXArray(unittest.TestCase):
         # expect passing test
         self.assertAlmostEqual(A.dens(20), 998.2, delta=tolerance)
 
-        # expect fail test
-        # with self.assertRaises(StandardError):
-            # A.dens(-10) # out of range
-
     def test_cp(self):
 
         """
@@ -77,11 +74,6 @@ class TestGHXArray(unittest.TestCase):
 
         # expect passing test
         self.assertAlmostEqual(A.cp(20), 4184.1, delta=tolerance)
-
-        # expect fail test
-        # with self.assertRaises(StandardError):
-            # A.cp(-10) # out of range
-
 
     def test_interp_g_funcs(self):
 
@@ -105,21 +97,198 @@ class TestGHXArray(unittest.TestCase):
         # extrapolate up
         self.assertAlmostEqual(A.g_func(5.0), 8.29, delta=tolerance)
 
+    def test_calc_ts(self):
+
+        """
+        Tests calc_ts which sets timescale
+        """
+
+        json_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', '1x2_Std_GHX.json')
+        csv_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', '1x2_Std_GHX.csv')
+
+        # init
+        A = ghx.GHXArray(json_file_path, csv_file_path, False) # pass 'False' to suppress output
+
+        A.calc_ts()
+
+        tolerance = 0.1
+
+        self.assertAlmostEqual(A.ts, 645858729.2, delta=tolerance)
+
     def test_AggregatedLoad_class(self):
 
         """
         Tests AggregatedLoad Class
         """
 
-        # init
-        A = ghx.AggregatedLoad([0,1,2,3,4,5,6,7,8,9], 10)
+        #init
+        A = ghx.AggregatedLoad([1,2,3,4,5,6,7,8,9,10], 10, 0)
 
         # check average load
-        self.assertEqual(A.q, 4.5)
+        self.assertEqual(A.q, 5.5)
 
         # check time
-        self.assertEqual(A.time(), 10)
+        self.assertEqual(A.time(), 5)
+
+    def test_merge_agg_load_objs(self):
+
+        """
+        Tests merge_agg_load_objs, which merges AggregatedLoad objects into a single object
+        """
+
+        json_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', '1x2_Std_GHX.json')
+        csv_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', '1x2_Std_GHX.csv')
+
+        # init
+        A = ghx.GHXArray(json_file_path, csv_file_path, False) # pass 'False' to suppress output
+
+        # make a few dummy AggregatedLoad classes
+        obj_1 = ghx.AggregatedLoad([1,2,3,4,5,6,7,8,9,10], 10, 0)
+        obj_2 = ghx.AggregatedLoad([1,2,3,4,5,6,7,8,9,10], 20, 10)
+        obj_3 = ghx.AggregatedLoad([1,2,3,4,5,6,7,8,9,10], 30, 20)
+        obj_4 = ghx.AggregatedLoad([1,2,3,4,5,6,7,8,9,10], 40, 30)
+
+        obj_list = [obj_1, obj_2, obj_3, obj_4]
+
+        ret_obj = A.merge_agg_load_objs(obj_list)
+
+        # check average load
+        self.assertEqual(ret_obj.q, 5.5)
+
+        # check time
+        self.assertEqual(ret_obj.time(), 20)
+
+    def test_aggregate_load(self):
+
+        """
+        Tests aggregate_load which aggregates and merges aggregated objects
+        """
+
+        json_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', '1x2_Std_GHX.json')
+        csv_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'examples', '1x2_Std_GHX.csv')
+
+        # init
+        A = ghx.GHXArray(json_file_path, csv_file_path, False) # pass 'False' to suppress output
+
+        # should initialize empty first object for comparative purposes
+        # [0]
+        self.assertEqual(len(A.agg_load_objects), 1)
+
+        # add object from hours 1-5
+        # [0,5]
+        A.hourly_loads = deque([1,1,1,1,1])
+        A.aggregate_load(5)
+
+        # check number of objects
+        self.assertEqual(len(A.agg_load_objects), 2)
+
+        # check sim hours
+        self.assertEqual(A.agg_load_objects[0].ave_sim_hour, 0)
+        self.assertEqual(A.agg_load_objects[1].ave_sim_hour, 2.5)
+
+        # check load
+        self.assertEqual(A.agg_load_objects[0].q, 0)
+        self.assertEqual(A.agg_load_objects[1].q, 1)
+
+        # add object from hours 6-10
+        # [0,5,5]
+        A.hourly_loads = deque([2,2,2,2,2])
+        A.aggregate_load(10)
+
+        # check number of objects
+        self.assertEqual(len(A.agg_load_objects), 3)
+
+        # check sim hours
+        self.assertEqual(A.agg_load_objects[0].ave_sim_hour, 0)
+        self.assertEqual(A.agg_load_objects[1].ave_sim_hour, 2.5)
+        self.assertEqual(A.agg_load_objects[2].ave_sim_hour, 7.5)
+
+        # check load
+        self.assertEqual(A.agg_load_objects[0].q, 0)
+        self.assertEqual(A.agg_load_objects[1].q, 1)
+        self.assertEqual(A.agg_load_objects[2].q, 2)
+
+        # add object from hours 11-15
+        # first two 5 hour blocks collapse into one 10 hour block
+        # [0,10,5]
+        A.hourly_loads = deque([3,3,3,3,3])
+        A.aggregate_load(15)
+
+        # check number of objects
+        self.assertEqual(len(A.agg_load_objects), 3)
+
+        # check sim hours
+        self.assertEqual(A.agg_load_objects[0].ave_sim_hour, 0)
+        self.assertEqual(A.agg_load_objects[1].ave_sim_hour, 5.0)
+        self.assertEqual(A.agg_load_objects[2].ave_sim_hour, 12.5)
+
+        # check load
+        self.assertEqual(A.agg_load_objects[0].q, 0)
+        self.assertEqual(A.agg_load_objects[1].q, 1.5)
+        self.assertEqual(A.agg_load_objects[2].q, 3)
+
+        # add object from hours 16-20
+        # [0,10,5,5]
+        A.hourly_loads = deque([4,4,4,4,4])
+        A.aggregate_load(20)
+
+        # check number of objects
+        self.assertEqual(len(A.agg_load_objects), 4)
+
+        # check sim hours
+        self.assertEqual(A.agg_load_objects[0].ave_sim_hour, 0)
+        self.assertEqual(A.agg_load_objects[1].ave_sim_hour, 5.0)
+        self.assertEqual(A.agg_load_objects[2].ave_sim_hour, 12.5)
+        self.assertEqual(A.agg_load_objects[3].ave_sim_hour, 17.5)
+
+        # check load
+        self.assertEqual(A.agg_load_objects[0].q, 0)
+        self.assertEqual(A.agg_load_objects[1].q, 1.5)
+        self.assertEqual(A.agg_load_objects[2].q, 3)
+        self.assertEqual(A.agg_load_objects[3].q, 4)
+
+        # add object from hours 21-25
+        # second two 5 hour blocks collapse into one 10 hour block
+        # [0,10,10,5]
+        A.hourly_loads = deque([5,5,5,5,5])
+        A.aggregate_load(25)
+
+        # check number of objects
+        self.assertEqual(len(A.agg_load_objects), 4)
+
+        # check sim hours
+        self.assertEqual(A.agg_load_objects[0].ave_sim_hour, 0)
+        self.assertEqual(A.agg_load_objects[1].ave_sim_hour, 5.0)
+        self.assertEqual(A.agg_load_objects[2].ave_sim_hour, 15.0)
+        self.assertEqual(A.agg_load_objects[3].ave_sim_hour, 22.5)
+
+        # check load
+        self.assertEqual(A.agg_load_objects[0].q, 0)
+        self.assertEqual(A.agg_load_objects[1].q, 1.5)
+        self.assertEqual(A.agg_load_objects[2].q, 3.5)
+        self.assertEqual(A.agg_load_objects[3].q, 5)
+
+        # add object from hours 26-30
+        # first two 10 hour blocks collapse into one 20 hour block
+        # [0,20,5,5]
+        A.hourly_loads = deque([6,6,6,6,6])
+        A.aggregate_load(30)
+
+        # check number of objects
+        self.assertEqual(len(A.agg_load_objects), 4)
+
+        # check sim hours
+        self.assertEqual(A.agg_load_objects[0].ave_sim_hour, 0)
+        self.assertEqual(A.agg_load_objects[1].ave_sim_hour, 10.0)
+        self.assertEqual(A.agg_load_objects[2].ave_sim_hour, 22.5)
+        self.assertEqual(A.agg_load_objects[3].ave_sim_hour, 27.5)
+
+        # check load
+        self.assertEqual(A.agg_load_objects[0].q, 0)
+        self.assertEqual(A.agg_load_objects[1].q, 2.5)
+        self.assertEqual(A.agg_load_objects[2].q, 5.0)
+        self.assertEqual(A.agg_load_objects[3].q, 6.0)
 
 # allow execution directly as python tests/test_ghx.py
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
