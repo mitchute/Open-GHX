dd2a052e3a23998ab54a5a0a7eef3f5241053c0e
Matt Mitchell <matt.s.mitchell@okstate.edu>
2016-11-11 12:58:42 -0600

    Broken testing

--- test/__init__.py
+++ test/__init__.py
@@ -1,1 +1,6 @@
-
+from test_ghx_aggregated_load import *
+from test_ghx_base import *
+from test_ghx_constants import *
+from test_ghx_fluids import *
+from test_ghxArray_Euler import *
+from test_ghxArray_Lagrange import *
